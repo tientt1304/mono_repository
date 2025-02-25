26c9276cefd6af9aee1a2f97c216bb7a8f8c0a3f
Tien Truong <tientt1304@gmail.com>
2025-02-25 13:34:05 +0700

    feat(camera module): add camera list screen

---
+++ packages/camera/lib/src/common/router/app_router.gr.dart
@@ -0,0 +1,26 @@
+// dart format width=80
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+// **************************************************************************
+// AutoRouterGenerator
+// **************************************************************************
+
+// ignore_for_file: type=lint
+// coverage:ignore-file
+
+part of 'app_router.dart';
+
+/// generated route for
+/// [CameraListScreen]
+class CameraList extends PageRouteInfo<void> {
+  const CameraList({List<PageRouteInfo>? children}) : super(CameraList.name, initialChildren: children);
+
+  static const String name = 'CameraList';
+
+  static PageInfo page = PageInfo(
+    name,
+    builder: (data) {
+      return const CameraListScreen();
+    },
+  );
+}
